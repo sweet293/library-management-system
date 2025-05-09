7746e5504dbaad156b5b63aa5f20eb6667fdf49a
Andreea Burdui <andreea.burdui@gmail.com>
2025-05-10 02:24:17 +0300

    added filtering feature and some design retouche

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import React from 'react';
 import Navbar from '@/components/Navbar/Navbar.jsx';
 import Hero from '@/components/Hero/Hero.jsx';
 import BookSection from '@/components/BookSection/BookSection.jsx';
+import lines from "./assets/website/lines.jpg"
+
 
 function App() {
   return (
     <div className="library-app">
       <Navbar />
+      <div className="relative -mt-20 bg-cover bg-fixed" style={{ backgroundImage: `url(${lines})` }}>
+
       <Hero />
       <div className="min-h-screen bg-gray-100 overflow-auto">
         <BookSection
-        sectionTitle="Library Collection"
-        sectionDescription="Filter by genre or search for your favorite titles."
         />
       </div>
+    </div>
     </div>
   );
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar';
 import bookfront2 from "../../assets/website/bookfront2.png"
+import lines from "../../assets/website/lines.jpg"
+
 
 const Hero = () => {
   return (
-    <div className="relative -mt-30">
-      <Navbar  />
+    <div className="relative -mt-20 bg-cover bg-fixed" style={{ backgroundImage: `url(${lines})` }}>
+      
       {/* Hero Section */}
       <div className="relative mt-0 pt-0">
         <img
@@ -16,8 +18,8 @@ const Hero = () => {
         
         {/* Hero Text */}
         <div className="absolute inset-0 flex items-center justify-center">
-          <h1 className="text-white text-4xl md:text-7xl font-merriweather animate-slideUp">
-            ...
+          <h1 className="text-gray-900 text-4xl md:text-7xl font-medium tracking-wide animate-slideUp">
+            Alexandria's Library
           </h1>
         </div>
       </div>
